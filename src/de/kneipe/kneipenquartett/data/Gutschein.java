e55df333ac3105dcd0865aa7a94675c9120ae74a
Michael Schwarz <scmi1025 (at) hs-karlsruhe.de>
2013-11-25 21:00:48 +0100

    Gutschein impl

---
+++ src/de/kneipe/kneipenquartett/data/Gutschein.java
@@ -0,0 +1,41 @@
+package de.kneipe.kneipenquartett.data;
+
+import static de.kneipe.KneipenQuartett.jsonBuilderFactory;
+
+import java.io.Serializable;
+
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+
+public class Gutschein implements JsonMappable, Serializable {
+	// fuer Kneipe.toJsonObject()
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7603928897111081391L;
+	
+	public Long gid;
+	public boolean status;
+	public String beschreibung;
+	public Kneipe kneipe;
+	public Benutzer benutzer;
+	
+	
+		JsonObjectBuilder getJsonBuilderFactory() {
+			return jsonBuilderFactory.createObjectBuilder()
+			                         .add("gid", gid)
+			                         .add("status", status)
+			                         .add("beschreibung", beschreibung);
+		}
+		@Override
+		public JsonObject toJsonObject() {
+			return getJsonBuilderFactory().build();
+		}
+		@Override
+		public void fromJsonObject(JsonObject jsonObject) {
+			gid = Long.valueOf(jsonObject.getJsonNumber("gid").longValue());
+			beschreibung = jsonObject.getString("beschreibung");
+			status = jsonObject.getBoolean("status");
+
+		}
+}
