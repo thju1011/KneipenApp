bd3a03eefc0e15ec307a15fc5e44f41ad5a4c10b
Julia Thamasett <thju1011 (at) hs-karlsruhe.de>
2013-11-25 15:19:03 +0100

    test

---
+++ src/de/kneipe/kneipenquartett/data/Bewertung.java
@@ -0,0 +1,26 @@
+package de.kneipe.kneipenquartett.data;
+
+import java.io.Serializable;
+
+import javax.json.JsonObject;
+
+public class Bewertung implements JsonMappable, Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5591521793265277000L;
+
+	@Override
+	public JsonObject toJsonObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void fromJsonObject(JsonObject jsonObject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
